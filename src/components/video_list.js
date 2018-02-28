d748346f23acda36aaac84b22805311a5e9a11bc
Iggi <iggirex@gmail.com>
2018-02-28 15:56:30 -0700

    child to parent communication working

---
+++ src/components/video_list.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const Videolist = (props) => {
+
+    const videoItems = props.videos.map((video) => {
+        return (
+            <VideoListItem
+                onVideoSelect={props.onVideoSelect}
+                key={video.etag} 
+                video={video} 
+            />
+        )
+        
+    });
+
+    const videos = props.video;
+
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
+    );
+};
+
+export default Videolist;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
-//Core React functionality in React Library
-import React from 'react';
-//DOM rendering functionality is diff library: ReactDOM
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = "AIzaSyBsX85XkZym24Kioz9iqQXmSG2ktLtTBdY";
 
+class App extends Component {
+    constructor(props) {
+        super(props)
 
-//Create a new component. This component should produce 
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
 
-// const App = function(){
-//     return <div>Hi!</div>;
-// }
-//We pass AN INSTANCE TO REACT DOM,
-//not a class
-const App = () => {
-    return <div>Hi!</div>;
+        YTSearch({key: API_KEY, term: "surfboards"}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos}
+                />   
+            </div>
+        );
+    }
 }
 
-
-//JSX is this html-looking stuff, it's still JS
-//the real html will not look exaclty like this
-
-//Take this component's generated HTML and put it
-//on the page (in the DOM)
-// !!! THIS IS WRONG -- React library does not render to DOM
-// React.render(App);
-
-// Correct/, ReactDOM is the library that renders to the DOM
-// ReactDOM.render(App)
-//WRONG App is a class, classes do not get rendered
-
-//Correct -- <App></App> is an instance of App class
-// can be shortened to <App /> since theren's nothing inside
-//ReactDOM.render(<App />);   // ERROR TARGET containeer is not a DOM element
-
-const container = document.getElementsByClassName('container')
-//Correct, add second argument to specify where to put jsx
-ReactDOM.render(<App />, container[0]);
 ReactDOM.render(<App />, document.querySelector('.container'));
